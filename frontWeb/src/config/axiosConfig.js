973ca3efc00d8edcd6c7bad0a8ffba462c1fd616
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2024-06-16 18:02:31 -0300

    Update axiosConfig.js

--- frontWeb/src/config/axiosConfig.js
+++ frontWeb/src/config/axiosConfig.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+/*import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://deployment-teste-1.onrender.com',
@@ -19,6 +19,30 @@ instance.interceptors.request.use(
     (error) => {
         return Promise.reject(error);
     }
+);
+
+export default instance;
+*/
+// axiosConfig.js
+
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://seu-backend.com/api', // Substitua pela URL do seu backend
+  timeout: 5000,
+});
+
+instance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
 );
 
 export default instance;
