bdd14a9322ffec2e5a0df287008415c3eea84c08
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2024-06-16 19:28:17 -0300

    Update main.jsx

--- frontWeb/src/main.jsx
+++ frontWeb/src/main.jsx
@@ -1,4 +1,4 @@
-/*import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Login from "./pages/login.jsx";
@@ -93,53 +93,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-*/
 
-// App.jsx
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import '../components/style/content.css';
-import '../app.css';
-import SideBar from '../components/sideBar';
-import Reserva from './pages/Reserva'; // Ajuste o caminho conforme a estrutura de pastas
-import CadastroReserva from './pages/CadastroReserva';
-import Contrato from './pages/Contrato';
-import Carro from './pages/Carro';
-import Cliente from './pages/Cliente';
-import Cadastro from './pages/Cadastro';
-import CadastroCarro from './pages/CadastroCarro';
-import CadastroCliente from './pages/CadastroCliente';
-import CriarContrato from './pages/CriarContrato';
-import ReservaStep1 from './components/ReservaStep1';
-import ReservaStep2 from './components/ReservaStep2';
-import ReservaStep3 from './components/ReservaStep3';
-
-function App() {
-    return (
-        <main className='app'>
-            <SideBar />
-            <div className='content'>
-                <Routes>
-                    <Route path="/" element={<Navigate to="reserva" />} />
-                    <Route path="reserva" element={<Reserva />} />
-                    <Route path="cadastroreserva" element={<CadastroReserva />}>
-                        <Route path="" element={<Navigate to="reserva-passo-1" />} />
-                        <Route path="reserva-passo-1" element={<ReservaStep1 />} />
-                        <Route path="reserva-passo-2" element={<ReservaStep2 />} />
-                        <Route path="reserva-passo-3" element={<ReservaStep3 />} />
-                    </Route>
-                    <Route path="contrato" element={<Contrato />} />
-                    <Route path="carro" element={<Carro />} />
-                    <Route path="cliente" element={<Cliente />} />
-                    <Route path="cadastro" element={<Cadastro />} />
-                    <Route path="cadastrocarro" element={<CadastroCarro />} />
-                    <Route path="cadastrocliente" element={<CadastroCliente />} />
-                    <Route path="criarcontrato" element={<CriarContrato />} />
-                </Routes>
-            </div>
-        </main>
-    );
-}
-
-export default App;
 
