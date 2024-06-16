30b97578c204c5aa7967edf66e359016c3476482
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2024-06-16 18:03:17 -0300

    Update reserva.jsx

--- frontWeb/src/pages/reserva.jsx
+++ frontWeb/src/pages/reserva.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+/*import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import axios from '../config/axiosConfig'
@@ -64,6 +64,75 @@ const Reserva = () => {
             </div>
         </div>
     );
+};
+
+export default Reserva;
+*/
+// Reserva.jsx
+
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from '../config/axiosConfig';
+import Table from '../components/table';
+import '../pages/style/container.css';
+
+const Reserva = () => {
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('/reserva');
+          setData(response.data);
+        } catch (error) {
+          console.error('Erro na requisição:', error);
+        }
+      };
+
+      fetchData();
+    } else {
+      navigate('/'); // Redireciona para a página de login se não houver token
+    }
+  }, [navigate]);
+
+  const deleteReserva = async (id) => {
+    try {
+      await axios.delete(`/reserva/${id}`);
+      setData(data.filter(reserva => reserva._id !== id));
+    } catch (error) {
+      console.error('Erro ao deletar reserva:', error);
+    }
+  };
+
+  return (
+    <div className='container'>
+      <div className='itens-container'>
+        <Table 
+          goto={'/app/cadastroreserva'}
+          btnTxt={'Criar Reserva'}
+          title={'Reservas'}
+          trs={[
+            'Id',
+            'Cliente',
+            'Retirada',
+            'Devolução',
+            'Agência de Retirada',
+            'Agência de Devolução',
+            'Categoria do Veículo',
+            'Diária (R$)',
+            'Colaborador',
+            'Ações'
+          ]}
+          data={data}
+          onDelete={deleteReserva}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Reserva;
