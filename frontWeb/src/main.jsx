105dd7f26bced8b2efd8d633c3575e4234ff213d
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2024-06-16 17:36:06 -0300

    Update main.jsx

--- frontWeb/src/main.jsx
+++ frontWeb/src/main.jsx
@@ -16,29 +16,31 @@ import ReservaStep1 from './components/reservaStep1';
 import ReservaStep2 from './components/reservaStep2';
 import ReservaStep3 from './components/reservaStep3';
 
+const router = (
+  <Routes>
+    <Route path="/" element={<Login />} />
+    <Route path="/app/*" element={<App />}>
+      <Route path="/" element={<Navigate to="reserva" />} />
+      <Route path="reserva" element={<Reserva />} />
+      <Route path="cadastroreserva" element={<CadastroReserva />}>
+        <Route path="/" element={<Navigate to="reserva-passo-1" />} />
+        <Route path="reserva-passo-1" element={<ReservaStep1 />} />
+        <Route path="reserva-passo-2" element={<ReservaStep2 />} />
+        <Route path="reserva-passo-3" element={<ReservaStep3 />} />
+      </Route>
+      <Route path="contrato" element={<Contrato />} />
+      <Route path="carro" element={<Carro />} />
+      <Route path="cliente" element={<Cliente />} />
+      <Route path="cadastro" element={<Cadastro />} />
+      <Route path="cadastrocarro" element={<CadastroCarro />} />
+      <Route path="cadastrocliente" element={<CadastroCliente />} />
+      <Route path="criarcontrato" element={<CriarContrato />} />
+    </Route>
+  </Routes>
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="app" element={<App />}>
-          <Route path="" element={<Navigate to="reserva" />} />
-          <Route path="reserva" element={<Reserva />} />
-          <Route path="cadastroreserva" element={<CadastroReserva />}>
-            <Route path="" element={<Navigate to="reserva-passo-1" />} />
-            <Route path="reserva-passo-1" element={<ReservaStep1 />} />
-            <Route path="reserva-passo-2" element={<ReservaStep2 />} />
-            <Route path="reserva-passo-3" element={<ReservaStep3 />} />
-          </Route>
-          <Route path="contrato" element={<Contrato />} />
-          <Route path="carro" element={<Carro />} />
-          <Route path="cliente" element={<Cliente />} />
-          <Route path="cadastro" element={<Cadastro />} />
-          <Route path="cadastrocarro" element={<CadastroCarro />} />
-          <Route path="cadastrocliente" element={<CadastroCliente />} />
-          <Route path="criarcontrato" element={<CriarContrato />} />
-        </Route>
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+    <Router>{router}</Router>
+  </React.StrictMode>
 );
