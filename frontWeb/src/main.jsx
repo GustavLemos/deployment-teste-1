d0d1871d05a6a74edb88c988d026e1d66485b08d
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2024-06-16 20:13:56 -0300

    Update main.jsx

--- frontWeb/src/main.jsx
+++ frontWeb/src/main.jsx
@@ -1,4 +1,4 @@
-/*import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Login from "./pages/login.jsx";
@@ -93,99 +93,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-*/
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Ajuste o caminho conforme a estrutura de pastas
-import Login from "./pages/login";
-import Cadastro from './pages/cadastro';
-import Carro from './pages/carro';
-import CadastroCarro from './pages/cadastroCarro';
-import Cliente from './pages/cliente';
-import Reserva from './pages/reserva';
-import CadastroCliente from './pages/cadastroCliente';
-import CadastroReserva from './pages/cadastroReserva';
-import Contrato from './pages/contrato';
-import CriarContrato from './pages/criarContrato';
-import ReservaStep1 from './components/reservaStep1';
-import ReservaStep2 from './components/reservaStep2';
-import ReservaStep3 from './components/reservaStep3';
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "app",
-    element: <App />,
-    children: [
-      {
-        path: '',
-        element: <Navigate to="reserva" />
-      },
-      {
-        path: "reserva",
-        element: <Reserva />,
-      },
-      {
-        path: "cadastroreserva",
-        element: <CadastroReserva />,
-        children: [
-          {
-            path: '',
-            element: <Navigate to="reserva-passo-1" />
-          },
-          {
-            path: 'reserva-passo-1',
-            element: <ReservaStep1 />
-          },
-          {
-            path: 'reserva-passo-2',
-            element: <ReservaStep2 />
-          },
-          {
-            path: 'reserva-passo-3',
-            element: <ReservaStep3 />
-          }
-        ]
-      },
-      {
-        path: "contrato",
-        element: <Contrato />,
-      },
-      {
-        path: "carro",
-        element: <Carro />,
-      },
-      {
-        path: "cliente",
-        element: <Cliente />,
-      },
-      {
-        path: "cadastro",
-        element: <Cadastro />,
-      },
-      {
-        path: "cadastrocarro",
-        element: <CadastroCarro />
-      },
-      {
-        path: "cadastrocliente",
-        element: <CadastroCliente />
-      },
-      {
-        path: "criarcontrato",
-        element: <CriarContrato />
-      }
-    ],
-  }
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
 
