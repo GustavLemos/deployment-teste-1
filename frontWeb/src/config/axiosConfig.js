d7eace7008e6be2f503687010e9e82d2e7fdad36
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2024-06-16 18:00:05 -0300

    Update axiosConfig.js

--- frontWeb/src/config/axiosConfig.js
+++ frontWeb/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://deployment-teste-1.onrender.com',
     headers: {
         'Content-Type': 'application/json',
         'accept': 'application/json'
