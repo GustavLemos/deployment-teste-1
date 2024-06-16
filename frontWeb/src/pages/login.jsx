5c6ab1d5b09b3c938d477a9a6e6c5fe79a0dc37a
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2024-06-16 18:35:51 -0300

    Update login.jsx

--- frontWeb/src/pages/login.jsx
+++ frontWeb/src/pages/login.jsx
@@ -1,4 +1,4 @@
-/*import React from 'react';
+import React from 'react';
 
 import FormLogin from "../components/formLogin";
 
@@ -24,32 +24,4 @@ function Login() {
 }
 
 export default Login;
-*/
-import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Importe useNavigate para lidar com navegação
-import FormLogin from '../components/formLogin'; // Verifique o caminho e o nome do componente FormLogin
-import '../pages/style/login.css';
 
-const Login = () => {
-    const navigate = useNavigate(); // Hook para navegação
-
-    const handleLogin = () => {
-        // Simulação de autenticação (substitua com lógica real de autenticação)
-        const isAuthenticated = true; // Simulação de autenticação bem-sucedida
-
-        if (isAuthenticated) {
-            navigate('/cadastro'); // Navega para a página de reserva após o login
-        } else {
-            // Lógica para lidar com falha na autenticação, se necessário
-            console.error('Falha na autenticação');
-        }
-    };
-
-    return (
-        <div className="loginPage">
-            <FormLogin />
-        </div>
-    );
-};
-
-export default Login;
