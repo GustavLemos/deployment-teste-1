fecfaf24a0c08230471329f9e37d3aaaa8c12080
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2024-06-17 00:14:11 -0300

    Teste cadastro

--- frontWeb/src/pages/cadastro.jsx
+++ frontWeb/src/pages/cadastro.jsx
@@ -1,115 +1,75 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import FormCadastro from '../components/formUser';
+import ButtonCadastro from '../components/buttonCadastro';
+import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Cadastro = () => {
-  const [nome, setNome] = useState('');
-  const [cpf, setCPF] = useState('');
-  const [rg, setRG] = useState('');
-  const [telefone, setTelefone] = useState('');
-  const [dataNascimento, setDataNascimento] = useState('');
-  const [senha, setSenha] = useState('');
-  const [genero, setGenero] = useState('Masculino');
+function Cadastro() {
+    const [nome, setNome] = useState('');
+    const [cpf, setCPF] = useState('');
+    const [rg, setRG] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [dataNascimento, setDataNascimento] = useState('');
+    const [senha, setSenha] = useState('');
+    const [genero, setGenero] = useState('Masculino');
+    const [message, setMessage] = useState('');
 
-  const cadastrar = async () => {
-    try {
-      const token = await AsyncStorage.getItem('token');
-      const response = await axios.post(
-        'https://deployment-teste-1.onrender.com/colaborador', // URL da sua rota de cadastro
-        { nome, cpf, rg, telefone, dataNascimento, senha, genero }, // Dados do usuário
-        {
-          headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json',
-            'accept': 'application/json' // Usar o token gerado no login para autenticar a requisição
-          },
+    const cadastrar = async () => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const response = await axios.post(
+                'https://deployment-teste-1.onrender.com/colaborador', // URL da sua rota de cadastro
+                { nome, cpf, rg, telefone, dataNascimento, senha, genero }, // Dados do usuário
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json',
+                        'accept': 'application/json' // Usar o token gerado no login para autenticar a requisição
+                    },
+                }
+            );
+
+            console.log('Resposta da API:', response.data);
+            setMessage('Usuário cadastrado com sucesso!');
+            // Limpar os campos do formulário após o cadastro (opcional)
+            setNome('');
+            setCPF('');
+            setRG('');
+            setTelefone('');
+            setDataNascimento('');
+            setSenha('');
+            setGenero('Masculino');
+        } catch (error) {
+            console.error('Erro ao cadastrar usuário:', error);
+            setMessage('Erro ao cadastrar usuário. Verifique os dados e tente novamente.');
         }
-      );
+    };
 
-      console.log('Resposta da API:', response.data);
-      Alert.alert('Sucesso', 'Usuário cadastrado com sucesso!');
-      // Limpar os campos do formulário após o cadastro (opcional)
-      setNome('');
-      setCPF('');
-      setRG('');
-      setTelefone('');
-      setDataNascimento('');
-      setSenha('');
-      setGenero('Masculino');
-    } catch (error) {
-      console.error('Erro ao cadastrar usuário:', error);
-      Alert.alert('Erro', 'Erro ao cadastrar usuário. Verifique os dados e tente novamente.');
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome"
-        value={nome}
-        onChangeText={setNome}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="CPF"
-        value={cpf}
-        onChangeText={setCPF}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="RG"
-        value={rg}
-        onChangeText={setRG}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Telefone"
-        value={telefone}
-        onChangeText={setTelefone}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Data de Nascimento"
-        value={dataNascimento}
-        onChangeText={setDataNascimento}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        secureTextEntry={true}
-        value={senha}
-        onChangeText={setSenha}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Gênero"
-        value={genero}
-        onChangeText={setGenero}
-      />
-      <Button
-        title="Cadastrar"
-        onPress={cadastrar}
-      />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-  },
-  input: {
-    width: '100%',
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    marginBottom: 10,
-  },
-});
+    return (
+        <div className='userSign'>
+            <form onSubmit={handleSubmit}>
+                <div id='header-form'>
+                    <h2>Cadastro de Usuário</h2>
+                </div>
+                <FormCadastro id={"top"} placeholder={['Nome']} value={nome} onChange={(e) => setNome(e.target.value)} />
+                <div id='middle'>
+                    <FormCadastro id={"middle-left"} placeholder={['CPF', 'RG', 'Telefone']} value={cpf} onChange={(e) => setCPF(e.target.value)} />
+                    <FormCadastro id={"middle-right"} placeholder={['Senha']}>
+                        <input id='date' type='date' value={dataNascimento} onChange={(e) => setDataNascimento(e.target.value)} />
+                        <select value={genero} onChange={(e) => setGenero(e.target.value)}>
+                            <option value="Masculino">Masculino</option>
+                            <option value="Feminino">Feminino</option>
+                        </select>
+                    </FormCadastro>
+                </div>
+                <div id='button-container'>
+                    <ButtonCadastro text={"Cadastrar"} width={'65%'} type="submit" onPress={cadastrar} />
+                </div>
+            </form>
+            {message && <p>{message}</p>}
+        </div>
+    );
+}
 
 export default Cadastro;
